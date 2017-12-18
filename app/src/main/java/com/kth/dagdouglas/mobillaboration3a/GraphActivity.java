eb4b0957ea800d9c78ebdc83786380bb57076b12
dpham <dpham@kth.se>
2017-12-18 17:19:47 +0100

    graph

--- app/src/main/java/com/kth/dagdouglas/mobillaboration3a/GraphActivity.java
+++ app/src/main/java/com/kth/dagdouglas/mobillaboration3a/GraphActivity.java
@@ -1,11 +1,17 @@
 package com.kth.dagdouglas.mobillaboration3a;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -13,8 +19,25 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-public class GraphActivity extends AppCompatActivity {
+public class GraphActivity extends AppCompatActivity  implements SensorEventListener{
 
+
+    private Sensor accelerometer;
+
+
+    private GraphView graphView1;
+    private SensorManager sensorManager;
+
+    LineGraphSeries<DataPoint> seriesX;
+    LineGraphSeries<DataPoint> seriesY;
+
+    LineGraphSeries<DataPoint> seriesZ;
+
+
+    private DataPoint[] initX = new DataPoint[]{};
+
+
+    private  int dataCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +58,41 @@ public class GraphActivity extends AppCompatActivity {
             }
         });
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
-        });
-        graph.addSeries(series);
+        graphView1=findViewById(R.id.graph);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        seriesX= new LineGraphSeries<>(new  DataPoint[]{});
+        seriesX.setColor(Color.CYAN);
+        seriesY = new LineGraphSeries<>(new DataPoint[]{});
+        seriesY.setColor(Color.RED);
+        seriesZ = new LineGraphSeries<>(new DataPoint[]{});
+        seriesZ.setColor(Color.GREEN);
+        graphView1.addSeries(seriesX);
+        graphView1.addSeries(seriesY);
+        graphView1.addSeries(seriesZ);
+        graphView1.getViewport().setXAxisBoundsManual(true);
+        graphView1.getViewport().setMinX(0);
+        graphView1.getViewport().setMaxX(20);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        double sensorX = event.values[0];
+        double sensory = event.values[1];
+        double sensorz = event.values[2];
+        if(dataCount>20) {
+            seriesX.appendData(new DataPoint(dataCount, sensorX), true, 20);
+            seriesY.appendData(new DataPoint(dataCount, sensory), true, 20);
+            seriesZ.appendData(new DataPoint(dataCount, sensorz), true, 20);
+        }
+        dataCount++;
+        Log.i("tag", "onSensorChanged: "+event.values[0]+" " +dataCount);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
